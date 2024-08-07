fa2a615482ae6e76269b4435df98f934a914d237
Luther Isaboke <lusaboke@gmail.com>
2024-08-07 17:24:00 +0300

    created application template

---
+++ vite-checkers/src/components/Game.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const Game = () => {
+ 
+  return (
+    <div className="game-container">
+      <h2>Game Board</h2>
+      
+         
+    </div>
+  );
+};
+
+export default Game;
---
+++ vite-checkers/src/components/SignUp.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/SignUp.css";
+
+const SignUp = () => {
+ 
+  return (
+    <div className="signup-container">
+      <h2 className="signup-title">Sign Up</h2>
+    
+    </div>
+  );
+};
+
+export default SignUp;
