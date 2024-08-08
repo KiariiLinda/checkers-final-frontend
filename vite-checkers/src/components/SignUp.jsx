048ad836e73456aeb60f5f5af96413898afcffe3
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2024-08-08 15:11:21 +0300

    Finished front end for home page

--- vite-checkers/src/components/SignUp.jsx
+++ vite-checkers/src/components/SignUp.jsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-
 const SignUp = () => {
-
-
   return (
     <div className="signup-container">
       <h2 className="signup-title">Sign Up</h2>
-   
     </div>
   );
 };
