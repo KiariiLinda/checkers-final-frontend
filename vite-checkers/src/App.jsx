eb71cf959627c8d888d57b320b40527a771aed3f
Jalenzz <126164913+Jalenzzz@users.noreply.github.com>
2024-08-09 23:05:45 +0300

    NotFound page

--- vite-checkers/src/App.jsx
+++ vite-checkers/src/App.jsx
@@ -1,9 +1,11 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
 import Game from "./components/Game";
+import NotFound from "./components/NotFound"; // Import the NotFound component
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/game" element={<Game />} />
+          {/* Catch-all route for 404 Not Found */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </Router>
